0e5cfd126bb6a495001509619750cf9a6ff33e62
Aman Dwivedi <amandwivedi71124.com>
2024-10-30 14:02:27 +0530

    added

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const bodyParser = require('body-parser');
+const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 const habitRoutes = require('./routes/habitRoutes');
